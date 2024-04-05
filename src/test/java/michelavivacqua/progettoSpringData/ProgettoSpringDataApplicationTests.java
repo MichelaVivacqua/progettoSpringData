235c43bf8f03ff4c550d1e410753c746d66eb981
MichelaVivacqua <vivacquamichela@gmail.com>
2024-04-05 16:54:00 +0200

    Initial commit

---
+++ src/test/java/michelavivacqua/progettoSpringData/ProgettoSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package michelavivacqua.progettoSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
